be973f6ae02d400d4293018212d78897b4394144
Gerard <gerardmc12@gmail.com>
2019-05-01 17:34:57 +0200

    adding backgroundColor

--- LoginSignup/ForgotPass/ForgotPassword.swift
+++ LoginSignup/ForgotPass/ForgotPassword.swift
@@ -13,6 +13,7 @@ class ForgotPassword: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = BLUE_THEME
     }
     
 }//ForgotPassword
